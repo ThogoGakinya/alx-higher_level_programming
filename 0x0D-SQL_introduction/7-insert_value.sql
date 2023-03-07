e39a79d3bc2607cb6a19d04f184f921cb821346b
ThogoGakinya <samsongakinya3@gail.com>
2023-03-07 09:42:19 -0800

    "updated insert"

--- 0x0D-SQL_introduction/7-insert_value.sql
+++ 0x0D-SQL_introduction/7-insert_value.sql
@@ -1,3 +1,3 @@
---inserting record into the table first-table.
-INSERT INTO `first_table` (`id`, `name`) VALUES (89, "Best School");
+-- Inserts a new row into the table first_table.
+INSERT INTO `first_table` (`id`, `name`) VALUES (89, "Holberton School");
 
