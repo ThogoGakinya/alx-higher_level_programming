a357c76f9aa49fc4fec68721c9a632d6ceab3c34
ThogoGakinya <samsongakinya3@gail.com>
2023-03-12 12:16:23 -0700

    "more updates"

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -1,4 +1,4 @@
 --listing down the rights and permissions granted to users
 SHOW GRANTS FOR 'user_0d_1'@'localhost';
-SHOW GRANTS FOR 'user_0d_1'@'localhost';
+SHOW GRANTS FOR 'user_0d_2'@'localhost';
 
